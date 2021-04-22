2520adf09a5c8e3a4149e2b7b692e9d864945ee2
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-22 14:49:32 +0200

    layout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+/* 
+1` fare l'HTML con input e ul li
+2nel JS creare un arrai con stringhe
+3ciclo for dell['array per sampare a video gli elementi facendo un append dentro ul 
+4 al click del cestino elminare il parent(O)
+5 intercettare il heyup() dell'impunt ENTER per aggiungere l'elemnto della lista e ripulire l'imput
+*/
+var toDo_list = ["Go to the grocery store and buy food", "Practice daily work out", "Study your homework"];
+
+for(var i = 0; i< toDo_list.length; i++){
+  var cloned = $("#to-do ul li").clone();
+  
+  var list_item = toDo_list[i];
+  cloned.find('span').append(list_item)
+  
+
+
+}
+
+
+});
