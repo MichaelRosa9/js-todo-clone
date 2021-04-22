b15f9d5f7c6be3d62d8e28b9202b5d777134ff95
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-22 16:32:07 +0200

    input function

--- js/script.js
+++ js/script.js
@@ -9,18 +9,32 @@ $(function(){
 */
 var toDo_list = ["Go to the grocery store and buy food", "Practice daily work out", "Study your homework"];
 
-//faccio un ciclo per estrarre le stringe individuali da appendere dentro un li contenuto in un div class="template" che sta in fondo al documento HMTL ma che clono a sua volta per appendere dentro l' ul che sta dintro il div #to-do
+//faccio un ciclo per estrarre le stringe individuali da appendere dentro un li contenuto in un div class="template" che sta in fondo al main ma che clono a sua volta per appendere dentro l' ul che sta dintro il div #to-do
 for(var i = 0; i< toDo_list.length; i++){
   var list_item = toDo_list[i];
+  //definisco dentro un var il clone dell' li che sta dentro il div template che sta in fondo al main
   var clone = $(".template li").clone();
   
+  //appendo la lista clonata dentro l' ul
   $('#to-do ul').append(clone);
-  console.log(clone)
   
+  //appendo la stringa dell'array dentro il clone
   $(clone).find('.text').append(list_item)
-
-
 }
+
+$('#to-do input').keyup(function(event){
+  
+  if(event.which === 13){
+    var input = $('input').val();
+    var clone = $(".template li").clone();
+
+    $(".template li").clone();
+    $('#to-do ul').append(clone);
+    $(clone).find('.text').append(input)
+    $(this).val('');
+  }
+
+})
 
 
 });
